920575f7d6326580c6331eb3db421f86c92822ee
Fabian Deutsch <fabiand@fedoraproject.org>
2012-06-27 12:40:56 +0200

    tui login: Use new storboard technique
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- tcs/uinput_login_as_admin.py
+++ tcs/uinput_login_as_admin.py
@@ -10,29 +10,34 @@ import time
 sys.path.append(os.environ["IGOR_LIBDIR"])
 import common.input
 
-
 logger = logging.getLogger(__name__)
 
 
-login_seq = [
-    # User
-    "admin\n",
-    lambda: time.sleep(0.7),
+story = [
+    # Enter Nothing, wait 0 seconds, expect "Please Login" on screen
+    (None,                0, "Please login"),
+
+    # Enter …, wait … seconds, expect … on screen
+    ("admin\n",           1, "Password:"),
+
     # Password (taken from set admin password)
-    "ovirt\n",
-    lambda: time.sleep(4),
+    ("ovirt\n",           1, "Networking:")
 ]
 
-
 def main():
-    logger.debug("Starting login")
-    common.input.play(login_seq)
-    on_screen = common.input.is_regex_on_screen("Networking:")
-    lock_exists = os.path.exists("/tmp/ovirt-setup.tty1")
+    logger.debug("Starting simulated TUI login")
+    passed = False
 
-    if on_screen and lock_exists:
-        return 0
-    return 1
+    try:
+        passed = suits_storyboard(story):
+    except Exception e:
+        logger.warning(e.message)
+    logger.debug("Finished simulated TUI login")
+
+    # Check for the TUI lock
+    passed = passed and os.path.exists("/tmp/ovirt-setup.tty1")
+
+    return 1 if passed else 0
 
 if __name__ == "__main__":
     sys.exit(main())
