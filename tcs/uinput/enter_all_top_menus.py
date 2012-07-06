20afa3aad74f28a5c30cc5b96f514c7811443e1b
Fabian Deutsch <fabiand@fedoraproject.org>
2012-07-06 11:33:38 +0200

    tui: Add navigation test
    This testcase goes through all top-level menus and checks if a keyword
    appears.
    
    Change-Id: I90cf6be19b61d19781e45d4154efe6f49e3399db
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ tcs/uinput/enter_all_top_menus.py
@@ -0,0 +1,45 @@
+#!/bin/env python
+# -*- coding: utf-8 -*
+# vim: set sw=4:
+
+import sys
+import os
+import logging
+import time
+
+sys.path.append(os.environ["IGOR_LIBDIR"])
+import common.input
+
+KEY_DOWN = common.input.uinput.KEY_DOWN
+KEY_UP = common.input.uinput.KEY_UP
+
+
+story = [
+    # First page, expect "Power Off" to be on the screen
+    (None,                0, "<Power Off>"),
+]
+
+keywords = ["System Identification",
+            "Remote Access",
+            "Keyboard Layout Selection",
+            "Enable SNMP",
+            "Logrotate",
+            "NFS Location",
+            "iSCSI",
+            "CIM Access",
+            "RHEV-M Configuration",
+            "RHN Registration"]
+
+# Now press key down, and expext $txt to be on the screen for all keywords
+for txt in keywords:
+    story.append((KEY_DOWN, 2, txt))
+
+# Now go back up and don't expect anythiong special (None)
+for txt in keywords:
+    story.append((KEY_UP, 2, None))
+
+# At last expect "<Power Off>" to be on the screen again
+story.append((None, 0, "<Power Off>")
+
+if __name__ == "__main__":
+    common.input.Storyboard("Enter all top menus and go back", story).run_and_exit()
