bcff2bd3a56a81e0ca4d36f4374b78d6213fd806
Fabian Deutsch <fabiand@fedoraproject.org>
2012-07-10 19:53:06 +0200

    tui installation: Update to match TUI flow
    Change-Id: I1a12075e3d754a2312b815a1f51ea4f6bdeecb1f
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- tcs/uinput/basic_tui_installation.py
+++ tcs/uinput/basic_tui_installation.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 story = [
     # P. 1 Welcome
     # Press nothing, wait 0 seconds, expect "Install …"
-    (None,                0, "Install Hypervisor"),
+    (None,                  0, "Install Hypervisor"),
 
     # P. 2: Enter keyboard selection
     # Press ENTER, wait 4 seconds, expect "Keyboard …"
@@ -25,7 +25,7 @@ story = [
 
     # P. 2: Select german keyboard layout
     # Press 53 times UP, wait 0 seconds and expect "German"
-    (53 * [common.input.uinput.KEY_UP], 0, "German"),
+    (39 * [common.input.uinput.KEY_UP], 2, "German"),
 
     # P. 3: Enter boot device selection
     # Press ENTER wait 4 seconds and expect "booting …"
@@ -36,13 +36,13 @@ story = [
 
     # P. 5: Enter password dialog
     (["\t\t\t\n"],          4, "Require a password"),
-    (["ovirt\tovirt\t"],    1, "a weak password"),
+    (["ovirt\tovirt\t"],    2, "a weak password"),
 
     # P. 6: Start installation
-    (["\t\t\n"],          120, "Installation Finished"),
+    (["\t\t\n"],          240, "Installation Finished"),
 
     # P. 7: Reboot
-    (["\n"], 0, None)
+    (["\n"],                0, None)
 ]
 
 if __name__ == "__main__":
